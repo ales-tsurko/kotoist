24cda2fb09a783b16c9f33331dfa4a1ea328e27c
Ales Tsurko <ales.tsurko@gmail.com>
2021-05-23 16:25:49 +0300

    Add logger

--- src/lib.rs
+++ src/lib.rs
@@ -17,19 +17,61 @@
     unreachable_pub
 )]
 
+use std::sync::Once;
+
+use log::{error, info, trace, LevelFilter};
+
 use vst::api::Supported;
-use vst::plugin::{CanDo, Category, Info, Plugin};
+use vst::plugin::{CanDo, Category, HostCallback, Info, Plugin};
 use vst::plugin_main;
 
+#[cfg(debug_assertions)]
+static ONCE: Once = Once::new();
+
 #[derive(Default)]
-struct Kotoist;
+struct Kotoist {
+    host: HostCallback,
+    sample_rate: f32,
+    block_size: i64,
+    is_playing: bool,
+}
 
 impl Plugin for Kotoist {
+    fn new(host: HostCallback) -> Self {
+        Self {
+            host,
+            ..Default::default()
+        }
+    }
+
+    fn init(&mut self) {
+        #[cfg(debug_assertions)]
+        init_log();
+    }
+
+    fn set_sample_rate(&mut self, rate: f32) {
+        self.sample_rate = rate;
+    }
+
+    fn set_block_size(&mut self, size: i64) {
+        self.block_size = size;
+    }
+
+    fn resume(&mut self) {
+        info!("hello");
+        self.is_playing = true;
+    }
+
+    fn suspend(&mut self) {
+        info!("suspend");
+        self.is_playing = false;
+    }
+
     fn get_info(&self) -> Info {
         Info {
             name: "Kotoist".to_string(),
             vendor: "Ales Tsurko".to_string(),
-            unique_id: 2742,
+            unique_id: 4269,
             category: Category::Generator,
             ..Default::default()
         }
@@ -54,6 +96,35 @@ impl Plugin for Kotoist {
             Other(_) => Maybe,
         }
     }
+}
+
+#[cfg(debug_assertions)]
+fn init_log() {
+    ONCE.call_once(|| {
+        let path = format!("{}/Desktop/kotoist.log", std::env::var("HOME").unwrap());
+        _init_log(path);
+        info!("init log");
+    });
+}
+
+#[cfg(windows)]
+fn _init_log(path: String) {
+    use simple_logging;
+    simple_logging::log_to_file(path, LevelFilter::Trace).unwrap();
+}
+
+#[cfg(unix)]
+fn _init_log(path: String) {
+    use simplelog::{ConfigBuilder, WriteLogger};
+    use std::fs::OpenOptions;
+
+    let file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    let config = ConfigBuilder::new().set_time_to_local(true).build();
+    let _ = WriteLogger::init(LevelFilter::Trace, config, file).unwrap();
 }
 
 plugin_main!(Kotoist);
