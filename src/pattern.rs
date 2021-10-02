e4173cce579216f724b3649bb910551c33031899
Ales Tsurko <ales.tsurko@gmail.com>
2021-10-02 19:09:17 +0300

    Update player with EventPattern

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,12 +1,10 @@
 mod scale;
 
-use std::collections::hash_map::HashMap;
 use std::convert::TryFrom;
 use std::sync::{Arc, Mutex, RwLock};
 
 use koto::runtime::{
     runtime_error, DataMap, Value, ValueIterator, ValueIteratorOutput, ValueList, ValueMap,
-    ValueNumber,
 };
 use thiserror::Error;
 use vst::api::{EventType, MidiEvent, MidiEventFlags, TimeInfoFlags};
@@ -17,7 +15,7 @@ use crate::parameters::Parameters;
 use scale::Scale;
 
 pub(crate) fn make_module(player: Arc<Mutex<Player>>) -> ValueMap {
-    use Value::{Empty, Iterator, List, Map, Number};
+    use Value::{Empty, List, Map, Number};
 
     let mut result = ValueMap::new();
 
@@ -27,24 +25,38 @@ pub(crate) fn make_module(player: Arc<Mutex<Player>>) -> ValueMap {
         move |vm, args| match vm.get_args(args) {
             [Map(map), Number(quant)] => {
                 let quant = f64::from(quant);
+
                 match EventPattern::try_from(map) {
-                    Ok(pattern) => todo!(),
-                    Err(e) => runtime_error!("{}", e),
+                    Ok(pattern) => player_cl.lock().unwrap().schedule_pattern(pattern, quant),
+                    Err(e) => return runtime_error!("{}", e),
                 }
+
+                Ok(Empty)
             }
-            [List(list), Number(quant)] => Ok(Empty),
-            [Iterator(iterator), Number(quant)] => {
-                let quant = f64::from(quant);
-                let player = player_cl.lock().unwrap();
-                player.schedule_stream(
-                    EventStream::new(iterator.to_owned(), Arc::clone(&player.parameters)),
-                    quant,
-                );
+            [List(list), Number(quant)] => {
+                for item in list.clone().data().iter() {
+                    match item {
+                        Map(map) => match EventPattern::try_from(map) {
+                            Ok(pattern) => {
+                                //TODO parallel patterns
+                                todo!()
+                            }
+                            Err(e) => return runtime_error!("{}", e),
+                        },
+                        _ => {
+                            return runtime_error!(
+                                "pattern.midi_out: \
+                            Expected arguments: map or list of maps, quantization."
+                            )
+                        }
+                    }
+                }
+
                 Ok(Empty)
             }
             _ => runtime_error!(
                 "pattern.midi_out: \
-                Expected arguments: iterator or map or list of them, quantization."
+                Expected arguments: map or list of maps, quantization."
             ),
         }
     });
@@ -66,6 +78,120 @@ pub(crate) fn make_module(player: Arc<Mutex<Player>>) -> ValueMap {
     });
 
     result
+}
+
+#[derive(Default)]
+pub(crate) struct Player {
+    host: HostCallback,
+    parameters: Arc<Parameters>,
+    queued_stream: RwLock<Option<ScheduledEventPattern>>,
+    stream: RwLock<Option<ScheduledEventPattern>>,
+    wait_until: f64,
+    last_position: f64,
+}
+
+impl Player {
+    pub(crate) fn new(host: HostCallback, parameters: Arc<Parameters>) -> Self {
+        Self {
+            host,
+            parameters,
+            ..Default::default()
+        }
+    }
+
+    fn schedule_pattern(&self, pattern: EventPattern, quant: f64) {
+        let position = self.position();
+        let quant_samples = quant * self.beat_length();
+        let offset = quant_samples - (position % quant_samples);
+        let position = offset + position;
+        *self.queued_stream.write().unwrap() = Some(ScheduledEventPattern { position, pattern });
+    }
+
+    fn position(&self) -> f64 {
+        self.host.get_time_info(0).unwrap().sample_pos
+    }
+
+    fn beat_length(&self) -> f64 {
+        let time_info = self
+            .host
+            .get_time_info(TimeInfoFlags::TEMPO_VALID.bits())
+            .unwrap();
+        let beats_per_sec = time_info.tempo / 60.0;
+        time_info.sample_rate / beats_per_sec
+    }
+
+    pub(crate) fn tick(&mut self) -> Option<Vec<MidiEvent>> {
+        if !self.is_playing() {
+            self.wait_until = self.wait_until - self.last_position;
+            return None;
+        }
+
+        let position = self.position();
+
+        if position < self.last_position {
+            self.wait_until = self.wait_until - self.last_position;
+        }
+
+        self.last_position = self.position();
+
+        self.check_queued(self.last_position);
+
+        let result = if let Some(stream) = self.stream.get_mut().unwrap() {
+            if position < self.wait_until {
+                return None;
+            }
+            match stream.pattern.try_next() {
+                Ok(event) => event.map(|e| self.process_events(position, e)),
+                Err(e) => {
+                    self.parameters.append_console(&format!("{}", e));
+                    None
+                }
+            }
+        } else {
+            None
+        };
+
+        result.map(|event| {
+            let beat_length = self.beat_length();
+            let length = event.event.length * beat_length * event.event.dur - 1.0;
+            event.into_vst_midi(self.host.get_block_size() as f64, length)
+        })
+    }
+
+    fn is_playing(&self) -> bool {
+        if let Some(time_info) = self.host.get_time_info(0) {
+            return TimeInfoFlags::from_bits(time_info.flags)
+                .map(|val| val.contains(TimeInfoFlags::TRANSPORT_PLAYING))
+                .unwrap_or(false);
+        }
+
+        false
+    }
+
+    /// check if the queued pattern should play
+    fn check_queued(&mut self, position: f64) {
+        let queued = self.queued_stream.get_mut().unwrap();
+        if let Some(stream) = queued.take() {
+            if position >= stream.position {
+                *self.stream.get_mut().unwrap() = Some(stream);
+            } else {
+                *queued = Some(stream);
+            }
+        }
+    }
+
+    fn process_events(&mut self, position: f64, event: Event) -> ScheduledEvent {
+        let end = event.dur * self.beat_length();
+        let offset = position % end;
+        self.wait_until = position + end - offset;
+
+        ScheduledEvent { position, event }
+    }
+}
+
+struct ScheduledEventPattern {
+    position: f64,
+    pattern: EventPattern,
 }
 
 struct EventPattern {
@@ -443,266 +569,6 @@ impl Stream for StreamScale {
     }
 }
 
-#[derive(Default)]
-pub(crate) struct Player {
-    host: HostCallback,
-    parameters: Arc<Parameters>,
-    queued_stream: RwLock<Option<ScheduledEventStream>>,
-    stream: RwLock<Option<ScheduledEventStream>>,
-    wait_until: f64,
-    last_position: f64,
-}
-
-impl Player {
-    pub(crate) fn new(host: HostCallback, parameters: Arc<Parameters>) -> Self {
-        Self {
-            host,
-            parameters,
-            ..Default::default()
-        }
-    }
-
-    fn schedule_stream(&self, e_stream: EventStream, quant: f64) {
-        let position = self.position();
-        let quant_samples = quant * self.beat_length();
-        let offset = quant_samples - (position % quant_samples);
-        let position = offset + position;
-        *self.queued_stream.write().unwrap() = Some(ScheduledEventStream { position, e_stream });
-    }
-
-    fn position(&self) -> f64 {
-        self.host.get_time_info(0).unwrap().sample_pos
-    }
-
-    fn beat_length(&self) -> f64 {
-        let time_info = self
-            .host
-            .get_time_info(TimeInfoFlags::TEMPO_VALID.bits())
-            .unwrap();
-        let beats_per_sec = time_info.tempo / 60.0;
-        time_info.sample_rate / beats_per_sec
-    }
-
-    pub(crate) fn tick(&mut self) -> Option<Vec<MidiEvent>> {
-        if !self.is_playing() {
-            self.wait_until = self.wait_until - self.last_position;
-            return None;
-        }
-
-        let position = self.position();
-
-        if position < self.last_position {
-            self.wait_until = self.wait_until - self.last_position;
-        }
-
-        self.last_position = self.position();
-
-        self.check_queued(self.last_position);
-
-        let result = if let Some(stream) = self.stream.get_mut().unwrap() {
-            if position < self.wait_until {
-                return None;
-            }
-            stream
-                .e_stream
-                .next()
-                .map(|event| self.process_events(position, event))
-        } else {
-            None
-        };
-
-        result.map(|event| {
-            let beat_length = self.beat_length();
-            let length = event.event.length * beat_length * event.event.dur - 1.0;
-            event.into_vst_midi(self.host.get_block_size() as f64, length)
-        })
-    }
-
-    fn is_playing(&self) -> bool {
-        if let Some(time_info) = self.host.get_time_info(0) {
-            return TimeInfoFlags::from_bits(time_info.flags)
-                .map(|val| val.contains(TimeInfoFlags::TRANSPORT_PLAYING))
-                .unwrap_or(false);
-        }
-
-        false
-    }
-
-    /// check if the queued pattern should play
-    fn check_queued(&mut self, position: f64) {
-        let queued = self.queued_stream.get_mut().unwrap();
-        if let Some(stream) = queued.take() {
-            if position >= stream.position {
-                *self.stream.get_mut().unwrap() = Some(stream);
-            } else {
-                *queued = Some(stream);
-            }
-        }
-    }
-
-    fn process_events(&mut self, position: f64, event: Event) -> ScheduledEvent {
-        let end = event.dur * self.beat_length();
-        let offset = position % end;
-        self.wait_until = position + end - offset;
-
-        ScheduledEvent { position, event }
-    }
-}
-
-struct ScheduledEventStream {
-    position: f64,
-    e_stream: EventStream,
-}
-
-struct EventStream {
-    iterator: ValueIterator,
-    parameters: Arc<Parameters>,
-}
-
-impl EventStream {
-    fn new(iterator: ValueIterator, parameters: Arc<Parameters>) -> Self {
-        EventStream {
-            iterator,
-            parameters,
-        }
-    }
-
-    fn next_in_iterator(&mut self, iterator: &mut ValueIterator) -> Option<Event> {
-        if let Some(value) = iterator.next() {
-            match value {
-                ValueIteratorOutput::Value(val) => match val {
-                    Value::Map(koto_event) => match self.event_from_koto(&koto_event) {
-                        Ok(event) => return Some(event),
-                        Err(e) => self.post_error(e),
-                    },
-                    Value::Iterator(iterator) => {
-                        return self.next_in_iterator(&mut iterator.clone());
-                    }
-                    Value::Empty => return None,
-                    _ => self.post_error(EventStreamError::TypeError),
-                },
-                ValueIteratorOutput::ValuePair(_, _) => {
-                    self.post_error(EventStreamError::TypeError)
-                }
-                ValueIteratorOutput::Error(err) => {
-                    self.parameters.append_console(&format!("{}", err))
-                }
-            }
-        }
-
-        None
-    }
-
-    fn event_from_koto(&mut self, koto_event: &ValueMap) -> Result<Event, EventStreamError> {
-        let map = koto_event.data();
-        let keys = vec!["dur", "length", "channel", "note", "velocity"];
-        let values: Result<Vec<&Value>, EventStreamError> = keys
-            .iter()
-            .map(|k| {
-                map.get_with_string(k)
-                    .ok_or(EventStreamError::EventTypeError)
-            })
-            .collect();
-        let mut event = HashMap::new();
-        values?.into_iter().enumerate().for_each(|(n, val)| {
-            event.insert(keys[n], val);
-        });
-
-        self.event_from_map(event)
-    }
-
-    fn event_from_map(&self, map: HashMap<&str, &Value>) -> Result<Event, EventStreamError> {
-        let mut event = HashMap::new();
-        for (key, value) in map.iter() {
-            if *key == "note" {
-                continue;
-            }
-
-            let value = match value {
-                Value::Number(num) => match num {
-                    ValueNumber::F64(val) => *val,
-                    ValueNumber::I64(val) => *val as f64,
-                },
-                _ => return Err(EventStreamError::TypeError),
-            };
-
-            event.insert(*key, value);
-        }
-
-        let value =
-            self.handle_notes(map["note"], event["velocity"] as u8, event["channel"] as u8)?;
-
-        Ok(Event {
-            value,
-            dur: event["dur"],
-            length: event["length"],
-        })
-    }
-
-    fn handle_notes(
-        &self,
-        value: &Value,
-        velocity: u8,
-        channel: u8,
-    ) -> Result<Vec<EventValue>, EventStreamError> {
-        if self.is_rest(value)? {
-            return Ok(vec![EventValue::Rest]);
-        }
-
-        let value = match value {
-            Value::Number(num) => match num {
-                ValueNumber::F64(val) => vec![*val],
-                ValueNumber::I64(val) => vec![*val as f64],
-            },
-            Value::List(list) => self.handle_notes_list(list)?,
-            _ => return Err(EventStreamError::TypeError),
-        };
-
-        Ok(value
-            .iter()
-            .map(|nn| EventValue::Note(*nn as u8, velocity, channel))
-            .collect())
-    }
-
-    fn is_rest(&self, value: &Value) -> Result<bool, EventStreamError> {
-        if let Value::Str(val) = value {
-            if val.as_str() == "rest" {
-                return Ok(true);
-            }
-
-            return Err(EventStreamError::TypeError);
-        }
-
-        Ok(false)
-    }
-
-    fn handle_notes_list(&self, list: &ValueList) -> Result<Vec<f64>, EventStreamError> {
-        list.data()
-            .iter()
-            .map(|val| match val {
-                Value::Number(num) => match num {
-                    ValueNumber::F64(v) => Ok(*v),
-                    ValueNumber::I64(v) => Ok(*v as f64),
-                },
-                _ => Err(EventStreamError::TypeError),
-            })
-            .collect()
-    }
-
-    fn post_error(&self, error: EventStreamError) {
-        self.parameters.append_console(&format!("{}\n", error));
-    }
-}
-
-impl Iterator for EventStream {
-    type Item = Event;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut iterator = self.iterator.clone();
-        self.next_in_iterator(&mut iterator)
-    }
-}
-
 #[derive(Debug, Clone)]
 pub(crate) struct ScheduledEvent {
     position: f64,
@@ -752,12 +618,6 @@ pub(crate) enum EventValue {
 }
 
 #[derive(Debug, Error)]
-enum EventPatternError {
-    #[error("Unexpected type for key '{0}', expected type is '{1}")]
-    KeyTypeError(String, String),
-}
-
-#[derive(Debug, Error)]
 enum StreamError {
     #[error("Unexpected value type '{0}' in stream, expected type is '{1}")]
     ValueTypeError(String, String),
@@ -767,12 +627,4 @@ enum StreamError {
     IteratorError(String),
     #[error("Error in stream: {0}")]
     OtherError(String),
-}
-
-#[derive(Debug, Error)]
-enum EventStreamError {
-    #[error("Unexpected return value type from stream")]
-    TypeError,
-    #[error("Event type is wrong or incomplete")]
-    EventTypeError,
 }
