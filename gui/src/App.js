7966b4c96f1b5676c18cfc2f727e0f8e28e5110b
Ales Tsurko <ales.tsurko@gmail.com>
2021-10-16 16:09:54 +0300

    Add pads view

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect, useRef } from "react";
 import Editor from "@monaco-editor/react";
-import { FaHammer, FaBroom } from "react-icons/fa";
+import { FaHammer, FaBroom, FaTh } from "react-icons/fa";
 
 function App() {
   const [code, setCode] = useState("");
   const [consoleOut, setConsoleOut] = useState("console output");
   const didMountRef = useRef(false);
   const editorRef = useRef(null);
+  const [padsVisible, setPadsVisible] = useState(false);
 
   useEffect(() => {
     if (didMountRef.current) {
       // window.external.invoke("SEND_CONSOLE_OUT " + consoleOut);
     } else {
       didMountRef.current = true;
-      setCode(window.external.invoke("GET_CODE"));
-      setConsoleOut(window.external.invoke("GET_CONSOLE_OUT"));
+      // setCode(window.external.invoke("GET_CODE"));
+      // setConsoleOut(window.external.invoke("GET_CONSOLE_OUT"));
       window.addEventListener("SEND_CONSOLE_OUT", (e) =>
         setConsoleOut(e.detail)
       );
@@ -27,12 +28,16 @@ function App() {
 
   const onChange = (newValue) => {
     setCode(newValue);
-    window.external.invoke("SEND_CODE " + newValue);
+    // window.external.invoke("SEND_CODE " + newValue);
+  };
+
+  const onTogglePads = () => {
+    setPadsVisible(!padsVisible);
   };
 
   const onClearButtonClick = () => {
     setConsoleOut("");
-    window.external.invoke("SEND_CONSOLE_OUT ");
+    // window.external.invoke("SEND_CONSOLE_OUT ");
   };
 
   const onBuildButtonClick = () => {
@@ -40,30 +45,120 @@ function App() {
       .getModel()
       .getValueInRange(editorRef.current.getSelection());
     const block = selection.length > 0 ? selection : code;
-    window.external.invoke("EVAL_CODE " + block);
+    // window.external.invoke("EVAL_CODE " + block);
     // setConsoleOut(result);
   };
 
   return (
     <React.Fragment>
-      <Editor
-        width="100%"
-        height="330px"
-        defaultLanguage="coffeescript"
-        onChange={onChange}
-        value={code}
-        onMount={editorDidMount}
-        options={{ minimap: { enabled: false }, rulers: [80], tabSize: 2 }}
+      <div className="editor-container">
+        <Pads visible={padsVisible} />
+        <Editor
+          width="100%"
+          height="330px"
+          defaultLanguage="coffeescript"
+          onChange={onChange}
+          value={code}
+          onMount={editorDidMount}
+          options={{ minimap: { enabled: false }, rulers: [80], tabSize: 2 }}
+        />
+      </div>
+      <Toolbar
+        onTogglePads={onTogglePads}
+        onClear={onClearButtonClick}
+        onBuild={onBuildButtonClick}
       />
-      <Toolbar onClear={onClearButtonClick} onBuild={onBuildButtonClick} />
       <Console text={consoleOut} />
     </React.Fragment>
+  );
+}
+
+function Pads(props) {
+  const [selection, setSelection] = useState(0);
+
+  const onSelectionChange = (number) => {
+    setSelection(number);
+  };
+
+  const pads = Array(128)
+    .fill(0)
+    .map((_, n) => (
+      <Pad
+        key={n.toString()}
+        number={n}
+        onSelectionChange={onSelectionChange}
+        selected={selection === n}
+      />
+    ));
+
+  return (
+    <div className={`pads ${props.visible ? "pads-visible" : ""}`}>{pads}</div>
+  );
+}
+
+function Pad(props) {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  const pitches = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
+  const noteName =
+    pitches[props.number % pitches.length] + Math.floor(props.number / 12);
+
+  const onMouseDown = () => {
+    setIsMouseDown(true);
+  };
+
+  const onMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  const onMouseEnter = () => {
+    setIsHover(true);
+  };
+
+  const onMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  return (
+    <div className="pad">
+      <div
+        className={`pad-button ${
+          isHover && !isMouseDown ? "pad-button-hover" : ""
+        }`}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {noteName}
+      </div>
+      <div
+        className={`pad-selection ${props.selected ? "pad-selected" : ""}`}
+        onClick={() => props.onSelectionChange(props.number)}
+      ></div>
+    </div>
   );
 }
 
 function Toolbar(props) {
   return (
     <div className="toolbar">
+      <button onClick={props.onTogglePads}>
+        <FaTh />
+      </button>
       <button onClick={props.onBuild}>
         <FaHammer />
       </button>
@@ -75,7 +170,9 @@ function Toolbar(props) {
 }
 
 function Console(props) {
-  return <div className="console" dangerouslySetInnerHTML={{ __html: props.text }} />;
+  return (
+    <div className="console" dangerouslySetInnerHTML={{ __html: props.text }} />
+  );
 }
 
 export default App;
